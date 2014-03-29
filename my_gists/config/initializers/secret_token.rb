e8d08167f5fff1638a5889ded667a8606ca390ee
Szymon <leyas@wp.pl>
2014-03-29 21:39:49 +0100

    wygenerowanie rusztowania

---
+++ my_gists/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyGists::Application.config.secret_key_base = '59e89a29d73a6742762fad723e4d8c96da8bd4424018adad41f11aba65ba97d10cd23aac768c55ca00996f4b0a29c362c17008004581f5eda8bb6a2b669a7305'
