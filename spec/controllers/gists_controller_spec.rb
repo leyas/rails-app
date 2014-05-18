a996f0e189915a9a21624a18838d06b1955f1491
leyas <leyas@wp.pl>
2014-05-18 23:24:11 +0200

    1-4 (index and new, http 200, renders template

--- spec/controllers/gists_controller_spec.rb
+++ spec/controllers/gists_controller_spec.rb
@@ -12,11 +12,18 @@ describe GistsController do
       get :index
       expect(response).to render_template("index")
     end
-
-    #it "loads all of the gists into @gists" do
-    #  gist1, gist2 = Gist.create!, Gist.create!
-    #  get :index
-    #  expect(assigns(:gists)).to match_array([gist1, gist2])
-    #end
   end
+  
+  describe "GET #new" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :new
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+    it "renders the edit template" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  end
+  
 end
