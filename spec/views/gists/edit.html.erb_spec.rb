13cbe63f20b7aedb6466a3b2067c11c1254c936a
leyas <leyas@wp.pl>
2014-05-19 04:16:30 +0200

    16-30 views headers and label render include

---
+++ spec/views/gists/edit.html.erb_spec.rb
@@ -0,0 +1,85 @@
+require 'spec_helper'
+
+describe "gists/edit.html.erb" do
+  before do
+    assign(:gist, stub_model(Gist, snippet: "object HelloWorld extends App {println('Hello, world!')}", description: "World example in Scala (from wiki).", language: "scala"))
+  end
+
+  it "displays header" do
+    render
+    expect(rendered).to include("Editing gist")
+  end
+
+  it "displays lang headline" do
+    render
+    expect(rendered).to include("Lang")
+  end
+
+  it "displays description headline" do
+    render
+    expect(rendered).to include("Description")
+  end
+
+  it "renders _form partial" do
+    render
+    expect(view).to render_template(:partial => "_form", :count => 1)
+  end
+
+  it "displays Snippet" do
+    render
+    expect(rendered).to include("snippet")
+  end
+
+  it "displays snippet" do
+    render
+    expect(rendered).to include("Snippet")
+    expect(rendered).to include("snippet")
+  end
+
+  it "displays description" do
+    render
+    expect(rendered).to include("description")
+  end
+
+  it "displays description" do
+    render
+    expect(rendered).to include("Description")
+    expect(rendered).to include("description")
+  end
+
+  it "displays show image" do
+    render
+    expect(rendered).to include("Show")
+  end
+
+  it "displays back button" do
+    render
+    expect(rendered).to include("back")
+  end
+
+  it "displays back image" do
+    render
+    expect(rendered).to include("Back")
+  end
+  
+  it "displays back image" do
+    render
+    expect(rendered).to include("Edit")
+  end
+  
+  it "displays label gist" do
+    render
+    expect(rendered).to include("gist")
+  end
+
+  it "displays label my gists" do
+    render
+    expect(rendered).to include("gist")
+  end
+  it "displays second label my gists" do
+    render
+    expect(rendered).to include("gist")
+  end
+
+end
+
